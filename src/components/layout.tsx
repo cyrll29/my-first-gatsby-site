821415df2ed0217b74bbf92bc93fc88dfec0b52b
Christian | Cy <cvillasenior@gmail.com>
2025-02-02 18:21:07 +0800

    Finished Gatsby Tutorial Part 6

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -26,6 +26,9 @@ const Layout = ({ pageTitle, children }: any) => {
                     <li className={navLinkItem}>
                         <Link to='/about' className={navLinkText}>About</Link>
                     </li>
+                    <li className={navLinkItem}>
+                        <Link to='/blog' className={navLinkText}>Blog</Link>
+                    </li>
                 </ul>
             </nav>
             <main>
